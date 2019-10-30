88a053d0e93744cd900faafaab7cf96311ace7f2
ewoods15 <ewoods.v@rrghst.org>
2019-10-30 22:34:02 +0000

    Oct 30 2019

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
-$( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$( "document" ).ready( function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$("button").click(function()
+let input=
+)};
+function piglatin(){
+    let letter= "a"
+    let vowels= ["a","e","i","o","u"]
+    vowels.forEach(function(vowel){
+        if letter= "vowel" +
+        console.log()
+}
+    )
+
+}
 
 
 
-  
-  
-  
-  
+
+
+
 });
